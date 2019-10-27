a3376df71b517fa1bea896831b968db40ff7f993
Nico De Witte <nico.dewitte@outlook.com>
2019-10-27 09:23:10 +0100

    Generate result using table package

--- src/grade.js
+++ src/grade.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const ResultsTableGenerator = require('./lib/results_table_generator');
 
 program
   .version('0.0.1')
@@ -9,3 +10,11 @@ program
 program.parse(process.argv);
 
 console.log("Grading ...");
+
+let results = [
+  ['Student', 'Assignment 1 (15%)', 'Assignment 2 (40%)', 'Assignment 3 (45%)', 'Total'],
+  ['Marki', '3/6 passed', '1/8 passed', '1/2 passed', '14.7%'],
+  ['Danny', '6/6 passed', '7/8 passed', '1/2 passed', '94.5%']
+];
+
+console.log(ResultsTableGenerator.generate(results));
