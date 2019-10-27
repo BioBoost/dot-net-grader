7dbf29dd3eecffde908804bbdb059156a2414df3
Nico De Witte <nico.dewitte@outlook.com>
2019-10-27 08:54:37 +0100

    Add basics command line args

--- src/grade.js
+++ src/grade.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
+const program = require('commander');
+
+program
+  .version('0.0.1')
+  .option('-c, --config <file>', 'json config file', './grade.json');
+ 
+program.parse(process.argv);
+
 console.log("Grading ...");
